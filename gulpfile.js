eac94ec41a92c09069e9fbc46892f339996d5aff
Rakesh Kumar <rakeshpersonal@gmail.com>
2015-11-16 17:08:28 +0530

    added gulp task for lint and broswersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+'use strict';
+
 var gulp = require('gulp');
 var Log = require('log');
 var logger = new Log('info');
 var sass = require('gulp-ruby-sass');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
 var browserSync = require('browser-sync').create();
 
 // Static server
@@ -24,6 +28,14 @@ gulp.task('sass', function () {
         .on('error', sass.logError)
         .pipe(gulp.dest('app/css'))
         .pipe(browserSync.stream());
+});
+
+gulp.task('lint', function() {
+    return gulp.src([
+        'app/js/*.js'
+    ]).pipe(jscs())
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 });
 
 gulp.task('start', ['startServer']);
